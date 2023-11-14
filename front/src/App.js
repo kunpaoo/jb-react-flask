62ef59ce19226ac572e0d48b432bd6e7d20c6107
MS-Crexa <vthelym@gmail.com>
2023-11-14 00:35:28 -0800

    Navbar

--- front/src/App.js
+++ front/src/App.js
@@ -1,20 +1,7 @@
 import LogIn_Page from './LogIn_Page';
+
 function App() {
 
-  
-  
-
-<<<<<<< HEAD
-  useEffect(() => {
-    fetch("/api")
-    .then((res) => res.json())
-    .then((data) => {
-      setData(data);
-    })
-  },[])
-
-=======
->>>>>>> main
 
   return (
     /* BODY CONTAINER */
@@ -28,20 +15,16 @@ function App() {
      
 
     <LogIn_Page/>
-
-<<<<<<< HEAD
   
     </div>
     </div>
-=======
+
     
->>>>>>> main
-    
-    {/* END MAIN CONTENT */}
-    </div>
-    </div>
+    /* END MAIN CONTENT */
+  
     /* END BODY CONTAINER */
   );
 }
+
 
 export default App;
