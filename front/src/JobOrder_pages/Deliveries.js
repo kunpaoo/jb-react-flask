43eb1e9d06411325007305c27c8d31fa1671b81e
kunpaoo <txcokun@gmail.com>
2023-11-27 00:10:26 +0800

    calendar modal fix

--- front/src/JobOrder_pages/Deliveries.js
+++ front/src/JobOrder_pages/Deliveries.js
@@ -9,62 +9,70 @@ import Navbar from "../Navbar";
 
 
 const Deliveries = () => {
-    const [modalIsOpen, setModalIsOpen] = useState(false);
-    const [eventInfo, setEventInfo] = useState({
-        title: "",
-        start: ""
-            });
+    
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [eventInfo, setEventInfo] = useState({title: "", date:""});
     
     const handleEventClick = (arg) => {
-        setModalIsOpen(true);
+        setIsModalOpen(true);
         setEventInfo({
             title: arg.event.title,
-            start: arg.event.start.toISOString(), // Convert date to string
+            date: arg.event.start.toISOString(), // Convert date to string
             })
     };
 
+    const closeModal = () => {
+        setIsModalOpen(false);
+        setEventInfo({
+            title:"",
+            date:""
+        }
+        )
+    }
+
 
     return (
+
     <div id="page-top" class="overflow-hidden">
-        <div id="wrapper">
-        <Navbar />
-        <div className="h-100 w-100">
-        <Header />
-        <div>
-        <div className="m-4">
-            <h2 className="mb-3">View Deliveries</h2>
-            <div className="d-sm flex justify-content-center">
-            <div className="maincontainer form-control">
-            <FullCalendar
-            plugins={[dayGridPlugin, interactionPlugin]}
-            initialView="dayGridMonth"
-            eventClick={this.handleEventClick}
-            events={[
-                { title: "event 1", date: "2023-11-13" },
-                { title: "event 2", date: "2023-11-15" },
-            ]}
-            />
+    <div id="wrapper">
+    <Navbar />
+    <div className="h-100 w-100">
+    <Header />
     
-            <Modal show={this.state.showModal} onHide={this.closeModal}>
-            <Modal.Header closeButton>
-                <Modal.Title>{this.state.eventInfo.title}</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <p>Start Date: {this.state.eventInfo.start}</p>
-                {/* Add other event information or content here */}
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant="secondary" onClick={this.closeModal}>
-                Close
-                </Button>
-            </Modal.Footer>
-            </Modal>
-            </div>
-            </div>
-        </div>
-        </div>
-        </div>
-        </div>
+    <div className="m-4">
+    <h2 className="mb-3">View Deliveries</h2>
+    <div className="d-sm flex justify-content-center">
+    <div className="maincontainer form-control">
+    
+    <FullCalendar
+    plugins={[dayGridPlugin, interactionPlugin]}
+    initialView="dayGridMonth"
+    eventClick={(event) => handleEventClick(event)}
+    events={[
+    { title: "event 1", date: "2023-11-13"},
+    { title: "event 2", date: "2023-11-15"}]} />
+
+
+    <Modal show={isModalOpen} onHide={closeModal} >
+    <Modal.Header closeButton>
+        <Modal.Title>{eventInfo.title}</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+        <p>Start Date: {eventInfo.date}</p>
+        {/* Add other event information or content here */}
+    </Modal.Body>
+    <Modal.Footer>
+        <Button variant="secondary" onClick={closeModal} >
+        Close
+        </Button>
+    </Modal.Footer>
+    </Modal> 
+
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
     </div>
     );
 }
