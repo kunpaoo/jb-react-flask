f3bb509c302108023d32489368b26185cb5e12f7
MS-Crexa <vthelym@gmail.com>
2023-11-26 12:47:40 +0800

    DCashier

--- front/src/Dashboard/DCashier.js
+++ front/src/Dashboard/DCashier.js
@@ -41,120 +41,87 @@ render(){
        </div>
 
        <div class="row">
-        <div class="col">
-         <div className="row me-1">
-          <div class="card">
-           <div class="card-body p-3">
-            <h3 class="card-title text-secondary">Quotations</h3>
-            <div className="row justify-content-between pt-2">
+        <div class="col me-3">
+         <div className="row">
+          <div className="card">
+           <div className="card-body">
+            <h4 className="card-title text-secondary">Quotations</h4>
+           </div>
+           <div className="col">
+            <div className="row">
              <div className="col-auto">
               <div>
-               <span className="text-lg">Total</span>
+               <span>Total</span>
               </div>
               <div>
-               <span>5</span>
+               <span>8</span>
               </div>
              </div>
              <div className="col-auto">
               <div>
-               <span className="text-lg">On hold</span>
+               <span>Accepted</span>
               </div>
               <div>
-               <span>5</span>
-              </div>
-             </div>
-             <div className="col-auto">
-              <div>
-               <span className="text-lg">Accepted</span>
-              </div>
-              <div>
-               <span>5</span>
+               <span>8</span>
               </div>
              </div>
              <div className="col-auto">
               <div>
-               <span className=" text-lg">Decline</span>
+               <span>To invoice</span>
               </div>
               <div>
-               <span>5</span>
+               <span>8</span>
               </div>
              </div>
              <div className="col-auto">
               <div>
-               <span className="text-lg">To invoice</span>
+               <span>Not Completed</span>
               </div>
               <div>
-               <span>5</span>
+               <span>8</span>
+              </div>
+              <div className="col-auto">
+               <div>
+                <span>Completed</span>
+               </div>
+               <div>
+                <span>8</span>
+               </div>
               </div>
              </div>
             </div>
            </div>
           </div>
+         </div>
 
-          <div className="row mt-2">
-           <div className="card">
-            <div className="card-body">
-             <div className="d-sm flex justify-content-center">
-              <div className="maincontainer form-control">
-               <FullCalendar
-                plugins={[dayGridPlugin, interactionPlugin]}
-                initialView="dayGridMonth"
-                eventClick={this.handleEventClick}
-                events={[
-                 { title: "event 1", date: "2023-11-13" },
-                 { title: "event 2", date: "2023-11-15" },
-                ]}
-               />
-
-               <Modal show={this.state.showModal} onHide={this.closeModal}>
-                <Modal.Header closeButton>
-                 <Modal.Title>{this.state.eventInfo.title}</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                 <p>Start Date: {this.state.eventInfo.start}</p>
-                 {/* Add other event information or content here */}
-                </Modal.Body>
-                <Modal.Footer>
-                 <Button variant="secondary" onClick={this.closeModal}>
-                  Close
-                 </Button>
-                </Modal.Footer>
-               </Modal>
-              </div>
-             </div>
-            </div>
-           </div>
-          </div>
-
-          <div className="row">
-           <div class="card mt-2">
-            <div class="card-body">
-             <h3 class="card-title text-secondary">Purchased History</h3>
-             <div className="row">
-              <div className="col">
-               <table className="table mt-3">
-                <thead>
-                 <tr>
-                  <th className="text-start">Id</th>
-                  <th className="text-start">Name</th>
-                  <th className="text-center">Brand</th>
-                  <th className="text-center">Unit</th>
-                  <th className="text-center">Quantity</th>
-                  <th className="text-center">Total Amount</th>
-                 </tr>
-                </thead>
-                <tbody>
-                 <tr>
-                  <td>1213</td>
-                  <td>Battery</td>
-                  <td>Acer</td>
-                  <td>123-jad1</td>
-                  <td>2</td>
-                  <td>$908</td>
-                 </tr>
-                </tbody>
-               </table>
-              </div>
+         <div className="row">
+          <div class="card mt-3">
+           <div class="card-body">
+            <h3 class="card-title text-secondary">Purchased History</h3>
+            <div className="row">
+             <div className="col">
+              <table className="table mt-3">
+               <thead>
+                <tr>
+                 <th className="text-start">Id</th>
+                 <th className="text-start">Name</th>
+                 <th className="text-center">Brand</th>
+                 <th className="text-center">Unit</th>
+                 <th className="text-center">Quantity</th>
+                 <th className="text-center">Total Amount</th>
+                </tr>
+               </thead>
+               <tbody>
+                <tr>
+                 <td>1213</td>
+                 <td>Battery</td>
+                 <td>Acer</td>
+                 <td>123-jad1</td>
+                 <td>2</td>
+                 <td>$908</td>
+                </tr>
+               </tbody>
+              </table>
              </div>
             </div>
            </div>
@@ -200,6 +167,41 @@ render(){
            <div class="card-body">
             <h6 class="card-title text-success">From OCCC</h6>
             <p class="card-text">Text</p>
+           </div>
+          </div>
+         </div>
+        </div>
+
+        <div className="col">
+         <div className="card">
+          <div className="card-body">
+           <div className="d-sm flex justify-content-center">
+            <div className="maincontainer form-control">
+             <FullCalendar
+              plugins={[dayGridPlugin, interactionPlugin]}
+              initialView="dayGridMonth"
+              eventClick={this.handleEventClick}
+              events={[
+               { title: "event 1", date: "2023-11-13" },
+               { title: "event 2", date: "2023-11-15" },
+              ]}
+             />
+
+             <Modal show={this.state.showModal} onHide={this.closeModal}>
+              <Modal.Header closeButton>
+               <Modal.Title>{this.state.eventInfo.title}</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+               <p>Start Date: {this.state.eventInfo.start}</p>
+               {/* Add other event information or content here */}
+              </Modal.Body>
+              <Modal.Footer>
+               <Button variant="secondary" onClick={this.closeModal}>
+                Close
+               </Button>
+              </Modal.Footer>
+             </Modal>
+            </div>
            </div>
           </div>
          </div>
